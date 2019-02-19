7fd78d77817d160300e5cce497f25e7e3bf2d29f
BABU, DINESH <dbabu@mast.com>
2019-02-19 08:37:25 +0530

    JavaInterview Questions

--- src/main/java/Array.java
+++ src/main/java/Array.java
@@ -7,8 +7,12 @@ public class Array {
 
     public static void main(String args[])
     {
-        PushanElementtoArray();
+        //PushanElementtoArray();
+        FindSecondLargest();
     }
+    /*
+         Insert an element in between the array
+     */
     public static void PushanElementtoArray()
     {
         int[] inputArray= new int[]{1,2,3,5,8,9};
@@ -24,7 +28,32 @@ public class Array {
         {
             outputArray[i]=inputArray[i-1];
         }
-        System.out.print("Original : "+ Arrays.toString(inputArray));
+        System.out.println("Original : "+ Arrays.toString(inputArray));
         System.out.print("Inserted : "+Arrays.toString(outputArray));
+    }
+
+    /*
+    Find Second largest number  in an array
+     */
+    public static void FindSecondLargest()
+    {
+        int[] array= {1,12,15,3,4,19,10};
+        int firstHighest= Integer.MIN_VALUE;
+        int secondHighest=Integer.MIN_VALUE;
+        for(int i=0;i< array.length;i++)
+        {
+                if( array[i] > firstHighest)
+                {
+                    secondHighest= firstHighest;
+                    firstHighest=array[i];
+                }
+                else if(array[i]>secondHighest)
+                {
+                    secondHighest=array[i];
+                }
+        }
+        System.out.println("Original : "+Arrays.toString(array));
+        System.out.println("First Highest : "+firstHighest);
+        System.out.println("Secong Highest : "+secondHighest);
     }
 }
